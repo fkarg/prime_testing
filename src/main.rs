025bc7cb0314d7f4832e61406a8dc2872182d341
fkarg <f.karg10@gmail.com>
2020-02-17 02:10:46 +0100

    wrote the code

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+extern crate primal;
+
+
+fn main() {
+    let counter: Vec<Vec<i32>> = vec![
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+
+    let (last, counter) = primal::Primes::all()
+        .take(100_000_000)
+        .fold( (1, counter), |(last, mut counter), n| {
+            counter[last % 10][n % 10] += 1;
+            (n, counter)
+        });
+
+    for (i, from) in counter.iter().enumerate() {
+        let mut sum = 0;
+        for to in from.iter() {
+            if to > &1 {
+                sum += to;
+            }
+        }
+
+        for (j, to) in from.iter().enumerate() {
+            if to > &1 {
+                println!("from {:?} to {:?}: {:?} ({:.5})", i, j, to, *to as f64 / sum as f64);
+            }
+        }
+
+        if sum > 0 {
+            println!("from {:?} to *: {:?}", i, sum);
+            println!();
+        }
+    }
+
+    println!("last: {:?}", last);
+}
