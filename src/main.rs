e21dd5fd9803b542788ae5bb35bce9b2f552782a
fkarg <f.karg10@gmail.com>
2020-02-22 13:53:08 +0100

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ extern crate primal;
 // Inspired from: https://www.quantamagazine.org/mathematicians-discover-prime-conspiracy-20160313/
 
 fn main() {
-    let counter: Vec<Vec<i32>> = vec![ vec![0; 10]; 10];
+    let counter: Vec<Vec<i32>> = vec![vec![0; 10]; 10];
 
-    let (last, counter) = primal::Primes::all()
-        .take(100_000_000)
-        .fold( (1, counter), |(last, mut counter), n| {
-            counter[last % 10][n % 10] += 1;
-            (n, counter)
-        });
+    let (last, counter) =
+        primal::Primes::all()
+            .take(100_000_000)
+            .fold((1, counter), |(last, mut counter), n| {
+                counter[last % 10][n % 10] += 1;
+                (n, counter)
+            });
 
     for (i, from) in counter.iter().enumerate() {
         let mut sum = 0;
@@ -22,7 +23,13 @@ fn main() {
 
         for (j, to) in from.iter().enumerate() {
             if to > &1 {
-                println!("from {:?} to {:?}: {:?} ({:.5})", i, j, to, *to as f64 / sum as f64);
+                println!(
+                    "from {:?} to {:?}: {:?} ({:.5})",
+                    i,
+                    j,
+                    to,
+                    *to as f64 / sum as f64
+                );
             }
         }
 
